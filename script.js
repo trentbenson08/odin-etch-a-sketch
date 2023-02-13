dc8cd90624ff5973ac48b0d8f89ca83922cc2bfb
trent <122744132+trentbenson08@users.noreply.github.com>
2023-02-13 15:20:33 -0600

    Edit index.html style.css script.js

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-const gridContainer = document.querySelector('#gridContainer');
-const CELL_TOTAL = 256;
-for(let i = 0; i<CELL_TOTAL; i++){
-    const newCell = document.createElement('div');
-    newCell.classList.add('gridCell')
-    gridContainer.appendChild(newCell)
+function main(){
+    createGrid();
+    const resizeGridButton = document.querySelector('#resizeGridButton');
+    resizeGridButton.addEventListener('click', () => {
+        gridSize = parseInt(prompt("Enter desired grid height/width:"));
+        const gridContainer = document.querySelector('#gridContainer');
+        const numOfChildren = document.querySelectorAll('.gridCell').length;
+        for(let i=0; i<numOfChildren; i++){
+            gridContainer.removeChild(gridContainer.lastElementChild);
+        }
+        createGrid(gridSize);
+    })
 }
 
-const cellList = document.querySelectorAll('.gridCell');
-cellList.forEach(element => {
-    element.addEventListener('mouseover', e => {
-        console.log(e.target);
-        e.target.style.background = 'red';
-    })
-});
+function createGrid(gridSize = 16){
+    const gridContainer = document.querySelector('#gridContainer');
+    let cellTotal = gridSize ** 2;
+    for(let i = 0; i<cellTotal; i++){
+        const newCell = document.createElement('div');
+        newCell.classList.add('gridCell');
+        newCell.style.cssText =`
+                                min-width : ${(960/gridSize).toFixed(1)}px;
+                                min-height : ${(960/gridSize).toFixed(1)}px;
+                                `;
+        gridContainer.appendChild(newCell);
+    }
+    const cellList = document.querySelectorAll('.gridCell');
+    cellList.forEach(element => {
+        element.addEventListener('mouseover', e => {
+            e.target.style.background = 'red';
+        })
+    });
+}
+
+main();
